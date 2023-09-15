1c7b7e7bc73cb726a50467086a54bfe2c5f0c066
DaniaAlogla <daniaalogla@gmail.com>
2023-09-15 14:38:07 +0300

    Initial Commit

---
+++ EssentialFeedMacOS/EssentialFeedMacOS.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedMacOS.h
+//  EssentialFeedMacOS
+//
+//  Created by Dania Alogla on 30/02/1445 AH.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedMacOS.
+FOUNDATION_EXPORT double EssentialFeedMacOSVersionNumber;
+
+//! Project version string for EssentialFeedMacOS.
+FOUNDATION_EXPORT const unsigned char EssentialFeedMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedMacOS/PublicHeader.h>
+
+
