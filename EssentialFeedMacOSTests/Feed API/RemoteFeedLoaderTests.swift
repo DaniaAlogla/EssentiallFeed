d4b025ae6ede8aa0350b5021a52b15417ec5dd72
DaniaAlogla <daniaalogla@gmail.com>
2023-12-12 12:21:47 +0300

    Simplify type declarations with a helper function

--- EssentialFeedMacOSTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedMacOSTests/Feed API/RemoteFeedLoaderTests.swift
@@ -38,13 +38,13 @@ class RemoteFeedLoaderTests : XCTestCase {
     func test_load_deliversErrorOnClientError(){
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
+        expect(sut, toCompleteWith: failure(.connectivity), when: {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         })
     }
     
-    func test_load_deliversErrorOnNon200HTTPResponce(){
+    func test_load_deliversErrorOnNon200HTTPResponse(){
         let (sut, client) = makeSUT()
         
         
@@ -53,7 +53,7 @@ class RemoteFeedLoaderTests : XCTestCase {
         samples.enumerated().forEach(){ index,code in
             
             
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code,data: json, at: index)
                 
@@ -65,7 +65,7 @@ class RemoteFeedLoaderTests : XCTestCase {
     func test_load_deliverErrorOn200ResponseWithInvalidJOSN(){
         let (sut,client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+        expect(sut, toCompleteWith: failure(.invalidData), when: {
             let invalidJOSN = Data(bytes: "invalid josn".utf8)
             client.complete(withStatusCode: 200, data: invalidJOSN)
         })
@@ -136,6 +136,11 @@ class RemoteFeedLoaderTests : XCTestCase {
         
         return (sut, client)
         
+    }
+    
+    
+    private func failure(_ error: RemoteFeedLoader.Error)-> RemoteFeedLoader.Result{
+        return .failure(error)
     }
     
     private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line){
