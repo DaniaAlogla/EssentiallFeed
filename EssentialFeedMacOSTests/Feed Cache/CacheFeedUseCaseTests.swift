6623d329fcf51e83555b324f487e571cfe081c64
DaniaAlogla <daniaalogla@gmail.com>
2024-02-16 19:01:59 +0300

    Add 'LocalFeedItem' data transfer representaion to decouple storage frameworks from 'FeedItem' data models

--- EssentialFeedMacOSTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedMacOSTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -42,11 +42,12 @@ class CacheFeedUseCaseTests: XCTestCase {
         let timestamp = Date()
         let (sut, store) = makeSUT(currentDate: { timestamp } )
         let items = [uniqueItem(),uniqueItem()]
-
+        let localItems = items.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
+        
         sut.save(items) { _ in }
         store.completionDeletionSuccessfully()
 
-        XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed,.insert(items, timestamp)])
+        XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed,.insert(localItems, timestamp)])
     }
     
     func test_save_failOnDeletionError(){
@@ -138,7 +139,7 @@ class CacheFeedUseCaseTests: XCTestCase {
         
         enum ReceivedMessage : Equatable {
             case deleteCacheFeed
-            case insert([FeedItem], Date)
+            case insert([LocalFeedItem], Date)
         }
         
         private(set) var receivedMessages = [ReceivedMessage]()
@@ -159,7 +160,7 @@ class CacheFeedUseCaseTests: XCTestCase {
             deletionCompletions[index](nil)
         }
         
-        func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion){
+        func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion){
             inertionCompletions.append(completion)
             receivedMessages.append(.insert(items, timestamp))
         }
