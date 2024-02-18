69aeb3b63d12942a09b5fce0bd5c85685469fb99
DaniaAlogla <daniaalogla@gmail.com>
2024-02-18 10:32:37 +0300

    Extract 'FeedCacheSpy' into a shared scope to remove duplication

---
+++ EssentialFeedMacOSTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,51 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeedMacOSTests
+//
+//  Created by Dania Alogla on 08/08/1445 AH.
+//
+
+import Foundation
+import EssentialFeedMacOS
+
+class FeedStoreSpy: FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    enum ReceivedMessage : Equatable {
+        case deleteCacheFeed
+        case insert([LocalFeedImage], Date)
+    }
+    
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var inertionCompletions = [InsertionCompletion]()
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCacheFeed)
+    }
+    
+    func completionDeletion(with error: Error, at index: Int = 0){
+        deletionCompletions[index](error)
+    }
+    
+    func completionDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion){
+        inertionCompletions.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0){
+        inertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0){
+        inertionCompletions[index](nil)
+    }
+
+}
