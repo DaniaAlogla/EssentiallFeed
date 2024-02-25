9b1d2901cedd2ecb4864ab577f542e87ea6696e1
DaniaAlogla <daniaalogla@gmail.com>
2024-02-25 15:42:52 +0300

    Segment functionalities into extensions

--- EssentialFeedMacOS/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeedMacOS/Feed Cache/LocalFeedLoader.swift
@@ -11,14 +11,29 @@ public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
     private let calender = Calendar(identifier: .gregorian)
-
+    
     public typealias LoadResult = LoadFeedResult
     public typealias SaveResult = Error?
-
+    
     public init (store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
     }
+    
+    private var maxCacheAgeInDays : Int {
+        return 7
+    }
+    
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCacheAge = calender.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return currentDate() < maxCacheAge
+    }
+    
+}
+
+extension LocalFeedLoader {
     
     public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         self.store.deleteCachedFeed { [weak self] error in
@@ -39,6 +54,9 @@ public final class LocalFeedLoader {
             completion(error)
         }
     }
+}
+
+extension LocalFeedLoader {
     
     public func load(completion: @escaping (LoadResult) -> Void){
         store.retrieve { [weak self] result in
@@ -55,6 +73,9 @@ public final class LocalFeedLoader {
             }
         }
     }
+}
+    
+extension LocalFeedLoader {
     
     public func validateCache() {
         self.store.retrieve { [weak self] result in
@@ -68,17 +89,6 @@ public final class LocalFeedLoader {
             case .empty, .found: break
             }
         }
-    }
-    
-    private var maxCacheAgeInDays : Int {
-        return 7
-    }
-    
-    private func validate(_ timestamp: Date) -> Bool {
-        guard let maxCacheAge = calender.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
-            return false
-        }
-        return currentDate() < maxCacheAge
     }
 }
 
