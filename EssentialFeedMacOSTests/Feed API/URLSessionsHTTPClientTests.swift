d051a0485fd6520c2a44502256466cf041d10dae
DaniaAlogla <daniaalogla@gmail.com>
2024-02-25 15:23:40 +0300

    Extract duplicate helpers into a shared scope

--- EssentialFeedMacOSTests/Feed API/URLSessionsHTTPClientTests.swift
+++ EssentialFeedMacOSTests/Feed API/URLSessionsHTTPClientTests.swift
@@ -142,16 +142,8 @@ class URLSessionsHTTPClientTests: XCTestCase {
         
     }
     
-    private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-    
     private func anyData() -> Data {
         return Data(bytes: "any data".utf8)
-    }
-    
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
     }
     
     private func anyHTTPURLResponse() -> HTTPURLResponse {
