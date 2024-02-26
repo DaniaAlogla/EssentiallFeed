b041611410d75b04ca87e949d2b28153311576e6
DaniaAlogla <daniaalogla@gmail.com>
2024-02-26 18:03:53 +0300

    Move 'FeedCachePolicy' to its own file

---
+++ EssentialFeedMacOS/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeedMacOS
+//
+//  Created by Dania Alogla on 16/08/1445 AH.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calender = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays : Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calender.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
