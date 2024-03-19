b6fe358de678f3bd95bd4486e49035b93ad2c87b
DaniaAlogla <daniaalogla@gmail.com>
2024-03-19 15:34:51 +0300

    Add 'EssentialFeedMacOSCacheIntegrationTests' target to sparate the potentially-slower cache integration tests from the fas unit/isolated tests

---
+++ EssentialFeedMacOSCacheIntegrationTests/EssentialFeedMacOSCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedMacOSCacheIntegrationTests.swift
+//  EssentialFeedMacOSCacheIntegrationTests
+//
+//  Created by Dania Alogla on 09/09/1445 AH.
+//
+
+import XCTest
+
+class EssentialFeedMacOSCacheIntegrationTests: XCTestCase {
+
+
+}
