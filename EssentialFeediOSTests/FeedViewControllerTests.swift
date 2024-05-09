e13554eb0e91b87d0263d39426b964f689cf4403
DaniaAlogla <daniaalogla@gmail.com>
2024-05-09 11:22:14 +0300

    Decouble tests from specigic UI controls for user initiated reloads with test-specific DSL methods

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -68,7 +68,7 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 1)
     }
     
-    func test_pullToRefresh_loadsFeed(){
+    func test_userInitiatedFeedReload_loadsFeed(){
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
@@ -99,7 +99,7 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
     
-    func test_pullToRefresh_showsLoadingIndicator() {
+    func test_userInitiatedFeedReload_showsLoadingIndicator() {
         let (sut, _) = makeSUT()
         
         sut.simulateUserInitiatedFeedReload()
@@ -107,7 +107,7 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
     }
     
-    func test_pullToRefresh_hidesLoadingIndicatorOnLoaderCompletion() {
+    func test_userInitiatedFeedReload_hidesLoadingIndicatorOnLoaderCompletion() {
         let (sut, loader) = makeSUT()
         
         sut.simulateUserInitiatedFeedReload()
