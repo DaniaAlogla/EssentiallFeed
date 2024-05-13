b6adf6d27243299d2e0a3dc8eb1e38d48f41e513
DaniaAlogla <daniaalogla@gmail.com>
2024-05-13 10:48:02 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Dania Alogla on 05/11/1445 AH.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
